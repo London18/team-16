5a35149872b0fc501eb15240d2e19a642f6bec5c
dizazta <khalilzaman@hotmail.com>
2018-11-10 00:59:32 +0000

    Added password handler

--- application.py
+++ application.py
@@ -9,8 +9,9 @@ def firstPage():
 @application.route('/', methods=['POST'])
 @application.route('/index', methods=['POST'])
 def my_form_post():
-    text = request.form['emailLogin']
-    return render_template('index.html', userEmail=str(text))
+    emailLogin = request.form['loginEmail']
+    emailPassword = request.form['loginPassword']
+    return render_template('index.html', userEmail=str(text), userPassword=str(emailPassword))
 
 if __name__ == "__main__":
     application.run()
