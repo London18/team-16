634dc93e0f00fb00824e0c624ecccb313459a133
dizazta <khalilzaman@hotmail.com>
2018-11-10 00:56:13 +0000

    Added handler for password in login

--- application.py
+++ application.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 application = Flask(__name__)
 
-@application.route("/")
+@application.route('/')
 @application.route('/index')
 def firstPage():
-    return render_template('index.html')
+    return render_template('index.html', userEmail="NOTHING")
+
+@application.route('/', methods=['POST'])
+@application.route('/index', methods=['POST'])
+def my_form_post():
+    text = request.form['emailLogin']
+    return render_template('index.html', userEmail=str(text))
 
 if __name__ == "__main__":
     application.run()
