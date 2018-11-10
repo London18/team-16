f5f468fcf1cba6f554649cc74bce37de9234dcbc
dizazta <khalilzaman@hotmail.com>
2018-11-10 03:57:52 +0000

    Next and previous buttons work

--- static/js/questionnaires/js-demographics.js
+++ static/js/questionnaires/js-demographics.js
@@ -47,7 +47,8 @@ $(document).ready(function(){
     // This function will figure out which tab to display
     var x = $('.tab');
     // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
+    //if (n == 1 && !validateForm()) return false;
+
     // Hide the current tab:
     $(x[currentTab]).css('display', 'none');
     // Increase or decrease the current tab by 1:
@@ -66,7 +67,7 @@ $(document).ready(function(){
     // This function deals with validation of the form fields
     var x, y, i, valid = true;
     x = $(".tab");
-    y = $(x[currentTab].find("input"));
+    y = $(x[currentTab]).find("input");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < y.length; i++) {
       // If a field is empty...
