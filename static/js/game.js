fe440c0ef3f25f80c3d1b523199b91d54fa1a565
szymon <syzmonsudol@gmail.com>
2018-11-10 10:04:37 +0000

    add really last one

--- static/js/game.js
+++ static/js/game.js
@@ -128,8 +128,8 @@ var mainState = {
     },
     getQuestion: function (player, question) {
         question.kill();
-        var questionaires = ["demographics.html", "employment_status.html", "organisational_culture.html"];
-        var rand = Math.floor(Math.random() * 3);
+        var questionaires = ["demographics.html", "employment_status.html", "organisational_culture.html", "open_ended.html"];
+        var rand = Math.floor(Math.random() * 4);
         window.open(questionaires[rand], '_blank');
         score += 5;
         scoreText.text = 'Score: ' + score;
